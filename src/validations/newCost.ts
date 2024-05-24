ee84186efa3b1542b72ad91f3c2894e65b676abe
MatiasMolinaDev <matiasnmolina1@gmail.com>
2024-05-24 20:22:23 -0300

    costs form validations

--- src/validations/newCost.ts
+++ src/validations/newCost.ts
@@ -2,8 +2,12 @@ import * as yup from 'yup'
 
 export function validationSchemaNewCost() {
   return yup.object({
-    title: yup.string().required(),
-    description: yup.string(),
-    amount: yup.number()
+    title: yup.string().required('Title is required'),
+    amount: yup.string().required('Amount is required'),
+    paidBy: yup.object({
+      id: yup.string().required(),
+      name: yup.string().required()
+    }),
+    participants: yup.array().min(1, 'At least one participant is required')
   })
 }
