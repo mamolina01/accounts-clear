fd5e9b9afb1c28a8f0acc9469aca0d130f578ada
MatiasMolinaDev <matiasnmolina1@gmail.com>
2024-05-29 20:28:19 -0300

    implement an emptyCosts component

--- src/components/home/groupSummary/groupSummary.tsx
+++ src/components/home/groupSummary/groupSummary.tsx
@@ -4,9 +4,14 @@ import { CostsList } from './costs/costsList/CostsList'
 import { useState } from 'react'
 import { Balances } from './balances/Balance'
 import { GroupProps } from '@/types/group'
+import { EmptyCosts } from '@/components'
 
 export const GroupSummary = ({ group }: { group: GroupProps }) => {
   const [tabActive, setTabActive] = useState('costs')
+
+  if (group.costs.length === 0) {
+    return <EmptyCosts />
+  }
 
   return (
     <div className="flex flex-col gap-4 bg-primary p-3 rounded">
