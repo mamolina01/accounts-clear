2b6a997827378a4702857a416c0ba7bf3dd1f394
MatiasMolinaDev <matiasnmolina1@gmail.com>
2024-06-13 16:40:10 -0300

    translate text to english

--- src/components/home/information/Information.tsx
+++ src/components/home/information/Information.tsx
@@ -1,4 +1,3 @@
-// import image1 from '@/public/coin.svg'
 import image1 from '@/public/images/money.jpg'
 import image2 from '@/public/images/friends2.jpg'
 import styles from './Information.module.scss'
@@ -7,12 +6,12 @@ import { InformationItem } from './InformationItem'
 export const Information = () => {
   return (
     <div className={styles.container}>
-      <InformationItem image={image1} title="Vacía tu cartera de recibos">
-        Todos los gastos se apuntan y todos los miembros del grupo pueden verlos.
+      <InformationItem image={image1} title="Keep your accounts up to date.">
+        Keep track of expenses and balances shared with roommates, travel companions, groups, friends, and family.
       </InformationItem>
-      <InformationItem image={image2} title="Una buena contabilidad genera buenos amigos" reverseColumn>
-        No más discusiones sobre la cuenta. <span className="text-primary font-semibold">Clear Accounts</span> te dice
-        quién es el siguiente en pagar, minimizando las transacciones.
+      <InformationItem image={image2} title="Good accounting makes good friends." reverseColumn>
+        With <span className="text-primary font-semibold">Clear Accounts</span>, split expenses with any group: trips,
+        roommates, friends, and family.
       </InformationItem>
     </div>
   )
