7c645d33f3f345bba86a85e8634b0e4de5aba95a
MatiasMolinaDev <matiasnmolina1@gmail.com>
2024-06-16 15:29:00 -0300

    change emptyCost location

--- src/components/Shared/index.ts
+++ src/components/Shared/index.ts
@@ -1,5 +1,4 @@
 export * from './header/Header'
 export * from './category/Category'
 export * from './containerForm/ContainerForm'
-export * from './emptyCosts/EmptyCosts'
 export * from './footer/Footer'
