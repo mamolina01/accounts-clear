1787e86e2a22e564a38f8d42bb56a2bab607eb66
MatiasMolinaDev <matiasnmolina1@gmail.com>
2024-07-06 21:14:19 -0300

    Update Tabs.test.tsx

--- src/__tests__/groupDetails/groupSummary/tabs/Tabs.test.tsx
+++ src/__tests__/groupDetails/groupSummary/tabs/Tabs.test.tsx
@@ -1,28 +1,32 @@
-import React from 'react'
-import { render } from '@testing-library/react'
+import React, { useState } from 'react'
+import { fireEvent, render } from '@testing-library/react'
 import { Tabs } from '@/components/groupDetails/groupSummary/tabs/Tabs'
-import userEvent from '@testing-library/user-event'
 
 describe('Tabs Component', () => {
   test('renders tabs with correct active state', async () => {
     // Arrange
     const setTabActive = jest.fn() // Mock función para cambiar el estado de la pestaña activa
-    const { getByRole } = render(<Tabs tabActive="costs" setTabActive={setTabActive} />)
+    const { getByText, rerender } = render(<Tabs tabActive="costs" setTabActive={setTabActive} />)
 
     // Act
-    const costsTab = getByRole('button', { name: /costs/i })
-    const balanceTab = getByRole('button', { name: /balance/i })
+    const costsTab = getByText('Costs')
+    const balanceTab = getByText('Balance')
 
     // Assert
-    expect(costsTab).toHaveClass('active') // Verificar que la pestaña 'Costs' esté activa por defecto
-    expect(balanceTab).not.toHaveClass('active') // Verificar que la pestaña 'Balance' no esté activa por defecto
+    expect(costsTab.parentNode).toHaveClass('active') // Verificar que la pestaña 'Costs' esté activa por defecto
+    expect(balanceTab.parentNode).not.toHaveClass('active') // Verificar que la pestaña 'Balance' no esté activa por defecto
 
     // Act: Simular clic en la pestaña 'Balance'
-    await userEvent.click(balanceTab)
+    fireEvent.click(balanceTab)
 
     // Assert
     expect(setTabActive).toHaveBeenCalledWith('balance') // Verificar que la función `setTabActive` se llama con 'balance' al hacer clic en la pestaña 'Balance'
-    expect(costsTab.classList.contains('active')).toBe(true)
-    expect(balanceTab).toHaveClass('active') // Verificar que la clase 'active' se ha añadido a la pestaña 'Balance'
+    expect(setTabActive).toHaveBeenCalledTimes(1)
+
+    // Update the state manually
+    rerender(<Tabs tabActive="balance" setTabActive={setTabActive} />)
+
+    expect(costsTab.parentNode).not.toHaveClass('active')
+    expect(balanceTab.parentNode).toHaveClass('active') // Verificar que la clase 'active' se ha añadido a la pestaña 'Balance'
   })
 })
