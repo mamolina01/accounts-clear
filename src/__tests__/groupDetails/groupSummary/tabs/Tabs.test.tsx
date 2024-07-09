8744787df0b4e5aaeb5f9c6df11db1f3792dd119
MatiasMolinaDev <matiasnmolina1@gmail.com>
2024-07-09 18:47:06 -0300

    test menu

--- src/__tests__/groupDetails/groupSummary/tabs/Tabs.test.tsx
+++ src/__tests__/groupDetails/groupSummary/tabs/Tabs.test.tsx
@@ -1,32 +1,25 @@
-import React, { useState } from 'react'
 import { fireEvent, render } from '@testing-library/react'
 import { Tabs } from '@/components/groupDetails/groupSummary/tabs/Tabs'
 
 describe('Tabs Component', () => {
-  test('renders tabs with correct active state', async () => {
-    // Arrange
-    const setTabActive = jest.fn() // Mock función para cambiar el estado de la pestaña activa
+  test('renders tabs with correct active state', () => {
+    const setTabActive = jest.fn()
     const { getByText, rerender } = render(<Tabs tabActive="costs" setTabActive={setTabActive} />)
 
-    // Act
     const costsTab = getByText('Costs')
     const balanceTab = getByText('Balance')
 
-    // Assert
-    expect(costsTab.parentNode).toHaveClass('active') // Verificar que la pestaña 'Costs' esté activa por defecto
-    expect(balanceTab.parentNode).not.toHaveClass('active') // Verificar que la pestaña 'Balance' no esté activa por defecto
+    expect(costsTab.parentNode).toHaveClass('active')
+    expect(balanceTab.parentNode).not.toHaveClass('active')
 
-    // Act: Simular clic en la pestaña 'Balance'
     fireEvent.click(balanceTab)
 
-    // Assert
-    expect(setTabActive).toHaveBeenCalledWith('balance') // Verificar que la función `setTabActive` se llama con 'balance' al hacer clic en la pestaña 'Balance'
+    expect(setTabActive).toHaveBeenCalledWith('balance')
     expect(setTabActive).toHaveBeenCalledTimes(1)
 
-    // Update the state manually
     rerender(<Tabs tabActive="balance" setTabActive={setTabActive} />)
 
     expect(costsTab.parentNode).not.toHaveClass('active')
-    expect(balanceTab.parentNode).toHaveClass('active') // Verificar que la clase 'active' se ha añadido a la pestaña 'Balance'
+    expect(balanceTab.parentNode).toHaveClass('active')
   })
 })
