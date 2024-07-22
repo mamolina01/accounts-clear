3e3885b28e73dc55df32a308f90566ccdbb03c8f
MatiasMolinaDev <matiasnmolina1@gmail.com>
2024-07-22 11:18:34 -0300

    group info translations

--- src/components/groupDetails/groupInfo/GroupInfo.tsx
+++ src/components/groupDetails/groupInfo/GroupInfo.tsx
@@ -5,12 +5,14 @@ import { GroupDetail } from '@/types/groupDetail'
 import { TopSection } from './topSection/TopSection'
 import styles from './GroupInfo.module.scss'
 import { Category } from './category/Category'
+import { useTranslations } from 'next-intl'
 
 interface Props {
   group: GroupDetail
 }
 
 export const GroupInfo = ({ group }: Props) => {
+  const t = useTranslations('groupDetails.groupInfo')
   const getUsers = () => {
     let users = group.participants.map(participant => participant.name).join(', ')
     return users
@@ -20,9 +22,9 @@ export const GroupInfo = ({ group }: Props) => {
     <div className={styles.container}>
       <TopSection group={group} />
       <div className={styles.leftContainer}>
-        <span className={styles.label}>Description</span>
+        <span className={styles.label}>{t('description')}</span>
         <span className={styles.description}>{group.description ? group.description : 'No description'}</span>
-        <span className={styles.label}>Participants</span>
+        <span className={styles.label}>{t('participants')}</span>
         <span className={styles.users}>{getUsers()}</span>
       </div>
       <div className={styles.rightContainer}>
