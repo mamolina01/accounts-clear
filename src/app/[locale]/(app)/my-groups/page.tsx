db3bd7c573d1d67b5e3f207f1454b819e7d6e43d
MatiasMolinaDev <matiasnmolina1@gmail.com>
2024-07-25 17:06:12 -0300

    My groups metadata title

--- src/app/[locale]/(app)/my-groups/page.tsx
+++ src/app/[locale]/(app)/my-groups/page.tsx
@@ -1,11 +1,22 @@
 import { getGroupListByUserId } from '@/actions/groups/get-group-list-by-userId'
 import { GroupList } from '@/components/groupList/GroupList'
+import { Locales } from '@/config/locales'
+import { Metadata, ResolvingMetadata } from 'next'
+import { useTranslations } from 'next-intl'
 
 interface Props {
   id: string
   name: string
   description: string
 }
+
+export function generateMetadata(parent: ResolvingMetadata): Metadata {
+  return {
+    title: 'My groups',
+    description: 'Groups list'
+  }
+}
+
 export default async function Groups() {
   // TODO: Order by date
   const groups: Props[] = await getGroupListByUserId()
